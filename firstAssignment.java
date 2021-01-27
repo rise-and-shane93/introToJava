b83981d8f9db7dedc454dce222750551daffaf7b
Shane Harper <cdtharper@gmail.com>
2021-01-27 18:13:29 -0500

    first commit

---
+++ firstAssignment.java
@@ -0,0 +1,13 @@
+public class firstAssignment {
+    public static void main(String[] args) {
+        // System.out.println("    J    A    V     V     A");
+        // System.out.println("    J   A A    V   V     A A");
+        // System.out.println("J   J  AAAAA    V V     AAAAA");
+        // System.out.println(" J J  A     A    V     A     A");
+
+        int x = 10;
+        System.out.println("The value of x is " + x);
+        //System.out.println(x);
+    }
+
+}
