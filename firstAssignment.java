6fc696762e242011e87d38395b94d1b23f725811
Shane Harper <cdtharper@gmail.com>
2021-01-29 20:29:12 -0500

    have program output 'java'

--- firstAssignment.java
+++ firstAssignment.java
@@ -1,13 +1,9 @@
 public class firstAssignment {
     public static void main(String[] args) {
-        // System.out.println("    J    A    V     V     A");
-        // System.out.println("    J   A A    V   V     A A");
-        // System.out.println("J   J  AAAAA    V V     AAAAA");
-        // System.out.println(" J J  A     A    V     A     A");
-
-        int x = 10;
-        System.out.println("The value of x is " + x);
-        //System.out.println(x);
+        System.out.println("    J    A    V     V     A");
+        System.out.println("    J   A A    V   V     A A");
+        System.out.println("J   J  AAAAA    V V     AAAAA");
+        System.out.println(" J J  A     A    V     A     A");
     }
 
 }
